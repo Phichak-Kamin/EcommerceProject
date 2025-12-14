13e29ebc066164d664616e6c364a082ee25af8d0
Phichak Limlamlertkul <phichak.limlamlertkul@gmail.com>
2025-12-14 17:09:33 +0700

    Creatadd to cart feature

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -16,7 +16,7 @@ export function CheckoutPage({ cart }) {
       );
       setDeliveryOptions(response.data);
 
-      response = axios.get(
+      response = await axios.get(
         '/api/payment-summary'
       );
       setPaymentSummary(response.data);
